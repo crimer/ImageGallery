e670163bdbe0f079a30ec24f0eee7dc3a9a75b36
crimer <99nik.pro@gmail.com>
2020-05-13 23:24:32 +1000

    add gallery page (view)

--- ImageGallery/ImageGallery/Controllers/GalleryController.cs
+++ ImageGallery/ImageGallery/Controllers/GalleryController.cs
@@ -1,23 +1,64 @@
 ﻿using ImageGallery.Data.Models;
+using ImageGallery.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ImageGallery.Controllers
 {
     public class GalleryController : Controller
     {
-        //public IActionResult Index()
-        //{
-            //var model = new Image()
-            //{
+        public IActionResult Index()
+        {
+            // создал пустые списки тэгов
+            List<Tag> hikingTags = new List<Tag>();
+            List<Tag> cityTags = new List<Tag>();
 
-            //};
-            //return model;
-        //}
-        
+            // создал три тэга
+            var tag1 = new Tag() { Id = 0, Description = "Boston" };
+            var tag2 = new Tag() { Id = 1, Description = "New York" };
+            var tag3 = new Tag() { Id = 2, Description = "Adventure" };
+
+            // заполнил списки тегов
+            hikingTags.Add(tag3);
+            cityTags.AddRange(new List<Tag>(){ tag1,tag2});
+
+            // заполнил список картинок
+            List<Image> imageList = new List<Image>()
+            {
+                new Image()
+                {
+                    Title = "Hiking Trip",
+                    ImageUrl = "https://images.unsplash.com/photo-1551632811-561732d1e306?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+                    Tags = hikingTags,
+                    Created = DateTime.Now
+                },
+                new Image()
+                {
+                    Title = "On The Trail",
+                    ImageUrl = "https://images.unsplash.com/photo-1480714378408-67cf0d13bc1b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+                    Tags = cityTags,
+                    Created = DateTime.Now
+                },
+                new Image()
+                {
+                    Title = "Downtown",
+                    ImageUrl = "https://images.unsplash.com/Ys-DBJeX0nE.JPG?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+                    Tags = cityTags,
+                    Created = DateTime.Now
+                }
+            };
+
+            // заполнил ViewModel
+            var model = new GalleryVM()
+            {
+                Images = imageList,
+                SearchQuery = ""
+            };
+
+            // вернул в представление ViewModel
+            return View(model);
+        }
+
     }
 }
