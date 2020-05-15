392f63e0624ac060257d0f7cb37831f066d7628f
crimer <99nik.pro@gmail.com>
2020-05-15 16:18:50 +1000

    add images folder and fill database

--- ImageGallery/ImageGallery/Controllers/GalleryController.cs
+++ ImageGallery/ImageGallery/Controllers/GalleryController.cs
@@ -17,44 +17,8 @@ namespace ImageGallery.Controllers
 
         public IActionResult Index()
         {
-            // создал пустые списки тэгов
-            //List<Tag> hikingTags = new List<Tag>();
-            //List<Tag> cityTags = new List<Tag>();
-
-            // создал три тэга
-            //var tag1 = new Tag() { Id = 0, Description = "Boston" };
-            //var tag2 = new Tag() { Id = 1, Description = "New York" };
-            //var tag3 = new Tag() { Id = 2, Description = "Adventure" };
-
-            // заполнил списки тегов
-            //hikingTags.Add(tag3);
-            //cityTags.AddRange(new List<Tag>(){ tag1,tag2});
-
             // заполнил список картинок
             var imageList = _imageService.GetAll();
-            //{
-            //    new Image()
-            //    {
-            //        Title = "Hiking Trip",
-            //        ImageUrl = "https://images.unsplash.com/photo-1551632811-561732d1e306?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-            //        Tags = hikingTags,
-            //        Created = DateTime.Now
-            //    },
-            //    new Image()
-            //    {
-            //        Title = "On The Trail",
-            //        ImageUrl = "https://images.unsplash.com/photo-1480714378408-67cf0d13bc1b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-            //        Tags = cityTags,
-            //        Created = DateTime.Now
-            //    },
-            //    new Image()
-            //    {
-            //        Title = "Downtown",
-            //        ImageUrl = "https://images.unsplash.com/Ys-DBJeX0nE.JPG?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-            //        Tags = cityTags,
-            //        Created = DateTime.Now
-            //    }
-            //};
 
             // заполнил ViewModel
             var model = new GalleryVM()
--- ImageGallery/ImageGallery/Startup.cs
+++ ImageGallery/ImageGallery/Startup.cs
@@ -59,7 +59,7 @@ namespace ImageGallery
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Gallery}/{action=Index}/{id?}");
             });
         }
     }
